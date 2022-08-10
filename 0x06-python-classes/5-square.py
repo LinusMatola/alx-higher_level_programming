b535580834861e48a92d9279626de6790e6b74e5
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-08-11 00:43:34 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square"""
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
+"""
 
+
+class Square:
+    """Square implementation
+    """
     def __init__(self, size=0):
-        """Initializes the data"""
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """Get the size"""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """Set the size"""
-        if type(value) != int:
+    def size(self, size):
+        if type(size) != int:
             raise TypeError('size must be an integer')
-        if value < 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = value
+        self.__size = size
 
     def area(self):
-        """Return the area of the square"""
-        return self.__size**2
+        """calculates the square area
+        """
+        return (self.__size ** 2)
 
     def my_print(self):
-        """Print the square"""
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                print('#' * self.__size)
+        """prints a square  with the corresponding size
+        """
+        if (self.__size == 0):
+            print('')
+
+        for l in range(self.__size):
+            print('#' * self.__size)
