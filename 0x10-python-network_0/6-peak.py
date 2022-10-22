9d7c865df1ee18acfc786799197fb295bf4938ba
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-10-22 05:16:04 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""
-Peak Module
-"""
-
-
-def divide(array, low, high):
-    """divide and conquer"""
-
-    mid = int((high + low)/2)
-    if array[mid-1] <= array[mid] >= array[mid+1]:
-        return array[mid]
-    elif array[mid] > array[mid+1]:
-        return divide(array, low, mid-1)
-    elif array[mid] < array[mid+1]:
-        return divide(array, mid+1, high)
+""" Write a function that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """Find peak of a list"""
-
+    """ Find a peak in a list of unsorted integers """
     if not list_of_integers:
         return None
-    return divide(list_of_integers, 0, len(list_of_integers)-1)
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] <= list_of_integers[mid + 1]:
+            low = mid + 1
+        elif list_of_integers[mid] <= list_of_integers[mid - 1]:
+            high = mid - 1
+        elif (list_of_integers[mid] >= list_of_integers[mid + 1] and
+              list_of_integers[mid] >= list_of_integers[mid - 1]):
+            return list_of_integers[mid]
+
+    return list_of_integers[low]
