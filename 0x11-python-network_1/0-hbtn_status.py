16d0e38adc670ecf489d2882671f512e2d876d78
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-10-23 05:29:21 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A script that
-fetches https://intranet.hbtn.io/status.
+fetches https://alx-intranet.hbtn.io/status
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
