d172b1849429470a30c4939a62a4819d9fb5c7ea
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-11-01 01:58:38 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const times = parseInt(process.argv[2]);
-if (isNaN(times)) {
+// script that prints x times “C is fun”
+const input = parseInt(process.argv[2]);
+if (!input) {
   console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < times; i++) {
-    console.log('C is fun');
-  }
+}
+for (let i = 0; i < input; i++) {
+  console.log('C is fun');
 }
