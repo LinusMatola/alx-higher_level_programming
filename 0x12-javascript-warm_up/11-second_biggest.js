15e7cafdd7778c5a16565d518b9840a68e9e3127
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-11-01 02:00:52 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
-
-if (args.length <= 1) {
-  console.log(0);
-} else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+//  script that searches the second biggest integer in the list of arguments
+let a = process.argv.map(Number);
+let mx = 0;
+let sc = 0;
+for (let i = 2; i < a.length; i++) {
+  if (a[i] > mx) {
+    sc = mx;
+    mx = a[i];
+  } else if (a[i] > sc && a[i] < mx) {
+    sc = a[i];
+  }
 }
+console.log(sc);
