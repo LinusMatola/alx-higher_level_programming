135ec4f88460aa9eab4667f164ae11579894d12e
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-11-29 02:32:41 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-
-const episodeId = process.argv[2];
-
-if (parseInt(episodeId) < 8) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
-
-  request(url, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    const { characters } = JSON.parse(body);
-
-    characters.forEach(
-      (url, index) => {
-        request(url, (err, res, body) => {
-          if (err) {
-            return console.log(err);
-          }
-          console.log(JSON.parse(body).name);
-        });
-      });
+const req = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+req(url, function (error, response, body) {
+  if (error) { console.log(error); }
+  JSON.parse(body).characters.forEach(function (peeps) {
+    req(peeps, function (error, response, body) {
+      if (error) { console.log(error); }
+      console.log(JSON.parse(body).name);
+    });
   });
-}
+});
