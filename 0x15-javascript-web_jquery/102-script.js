0cb3d4d883ea825dd96c567bb58e3e35db83f0b0
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-11-29 03:21:45 +0300

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-  $('#btn_search').on('click', function () {
-    let cityName = $('#city_search').val();
-    $('#city_search').val('');
+  $('INPUT#btn_translate').keypress(function (e) {
+    if (e.which === "Hello") {
+      $('#btn_translate').click();
+    }
+  });
 
-    let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + cityName + '%22)&format=json';
+  $('#btn_translate').on('click', function () {
+    let translatedName = $('#btn_translate').val();
+    $('#btn_translate').val('');
+
+    let url = 'https://www.fourtonfish.com/hellosalut?query=translatedName&format=json';
 
     $.get(url, (data) => {
-      $('DIV#sf_wind_speed').text(data.query.results.channel.wind.speed);
+      $('DIV#hello').text(data.query.results.channel.wind.speed);
     });
   });
 });
